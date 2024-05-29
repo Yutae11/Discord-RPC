3053bb44c890c4a5cbde62984a8e5c578f5ef0cb
Yutae11 <yutaokkotsu1110@gmail.com>
2024-05-29 17:16:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ module.exports = {
   token: "" || process.env.TOKEN,
   timeZone: "Asia/Kolkata", //Your Timezone, eg Asia/Kolkata
   Name: "My bio",
-  State: "CREATING WAIFUS",
+  State: "discord.js",
   Details: "DISCORD DEVELOPER",
   FirstButtonName: "ZERO TWO",
   FirstButtonUrl: "https://discord.com/oauth2/authorize?client_id=1239867862059974727",
   SecondButtonName: "DISCORD",
   SecondButtonUrl: "https://discord.com/invite/sbFJCnQzJz",
-  LargeImage: "https://cdn.discordapp.com/attachments/1219628338859999384/1243829578351509605/6965-zero-two-avatar-pfpsgg.gif?ex=6652e610&is=66519490&hm=1c32e9f1927d0272ffa05a176cf471444bad41947edc6841704dd21e92cdcd55&", // DISCORD CDN IMAGE ONLY
-  LargeText: "CREATING WAIFU", // hover text for large image
-  SmallImage: "https://cdn.discordapp.com/attachments/1219628338859999384/1243829888260112434/cute-smiling-zero-two-fybugb80h7plzr4r.webp?ex=6652e65a&is=665194da&hm=9b6255f7f4701224ef13f4486f094b91737258cb641698e64adb1ffde7a6f362&", // DISCORD CDN IMAGE ONLY
+  LargeImage: "https://cdn.discordapp.com/attachments/1219628338859999384/1245340997722832926/6965-zero-two-avatar-pfpsgg.gif?ex=665865af&is=6657142f&hm=42a3708557c1f00c7da2449337ea0167e036a24562688a36cc594f318748ef32&", // DISCORD CDN IMAGE ONLY
+  LargeText: "Creating waifus <3", // hover text for large image
+  SmallImage: "https://cdn.discordapp.com/attachments/1219628338859999384/1245341009408430181/IMG_20240525_004041_011.jpg?ex=665865b1&is=66571431&hm=24de38a33c1a4ae666b9fefdb67405241d893dc7c348ea8834f6fc5557ce71cc&", // DISCORD CDN IMAGE ONLY
   SmallText: "Zero two", // hover text for small image
 };
 
